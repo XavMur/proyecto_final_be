14108228511dbb183303ed38c9af97d189c68012
XavierMurillo <xaviermurillosanchez@gmail.com>
2024-05-12 19:33:51 -0400

    Added cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
 
 const {productosPorCategoria} = require('./consultas.js')
 
 app.listen(3000, console.log("Servidor funcionando"))
+app.use(cors())
 
 app.get('/productos', async(req, res)=>{
     const categorias = req.query.categoria
