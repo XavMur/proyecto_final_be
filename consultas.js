ae50c85a697c6b4aa8f7a6568c4b6df053105e62
Maur1998 <mmurillosanchez1998@gmail.com>
2024-05-12 19:37:29 -0400

    asddf

--- consultas.js
+++ consultas.js
@@ -1,31 +1,33 @@
 const format = require("pg-format");
-const {dbConnection} = require('./db_connection.js')
-const conn = dbConnection()
+const { dbConnection } = require("./db_connection.js");
+const conn = dbConnection();
 
-const getCategorias = async (categorias) =>{
-    consulta = "SELECT * FROM categorias";
-    try{
-        const {rows} = await conn.createConn().query(consulta);
-        catID = rows.filter(categoria =>categorias.includes(categoria.categoria)).map(categoria => categoria.id);
-        return catID;
-    }catch(error){
-        throw {"code": 404, "message": "Error al obtener las categorias"};
-    }
-}
+const getCategorias = async (categorias) => {
+  consulta = "SELECT * FROM categorias";
+  try {
+    const { rows } = await conn.createConn().query(consulta);
+    catID = rows
+      .filter((categoria) => categorias.includes(categoria.categoria))
+      .map((categoria) => categoria.id);
+    return catID;
+  } catch (error) {
+    throw { code: 404, message: "Error al obtener las categorias" };
+  }
+};
 
-const productosPorCategoria = async (categorias) =>{
-    const query = "SELECT * FROM productos WHERE categoria1 = ANY(ARRAY[%s]) \
+const productosPorCategoria = async (categorias) => {
+  const query =
+    "SELECT * FROM productos WHERE categoria1 = ANY(ARRAY[%s]) \
     OR categoria2 = ANY(ARRAY[%s]) OR categoria3 = ANY(ARRAY[%s])";
-    const categoriasID = await getCategorias(categorias);
-    const consulta = format(query, categoriasID, categoriasID, categoriasID);
-    try{
-        const {rows} = await conn.createConn().query(consulta);
-        console.log(rows)
-        return rows
-    } catch(error){
-        throw{"code": 404, "message": "Error al obtener los productos"};
-    }
-}
+  const categoriasID = await getCategorias(categorias);
+  const consulta = format(query, categoriasID, categoriasID, categoriasID);
+  try {
+    const { rows } = await conn.createConn().query(consulta);
+    console.log(rows);
+    return rows;
+  } catch (error) {
+    throw { code: 404, message: "Error al obtener los productos" };
+  }
+};
 
-
-module.exports = {productosPorCategoria}
+module.exports = { productosPorCategoria };
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const {productosPorCategoria} = require('./consultas.js')
+const { productosPorCategoria, getCategorias } = require("./consultas.js");
 
-app.listen(3000, console.log("Servidor funcionando"))
+app.listen(3000, console.log("Servidor funcionando"));
 
-app.get('/productos', async(req, res)=>{
-    const categorias = req.query.categoria
-    const productos = await productosPorCategoria(categorias)
-    res.json(productos)
+app.get("/productos", async (req, res) => {
+  const categorias = req.query.categoria;
+  const productos = await productosPorCategoria(categorias);
+  res.json(productos);
+});
 
-})
+app.get("/categorias", async (req, res) => {
+  const categorias = req.query.categoria;
+  const productos = await getCategorias(categorias);
+  res.json(productos);
+});
